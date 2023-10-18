299dceca3b9b382c5fc0fe193be451021dfea87a
subham8391 <subhamdas967990@gmail.com>
2023-10-18 13:19:24 +0530

    update commit

--- src/components/SightBarModal.jsx
+++ src/components/SightBarModal.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 import { AiFillCloseCircle } from "react-icons/ai";
 import { Link } from 'react-router-dom';
+import logo from '../images/zee5-logo-A4464FCB2C-seeklogo.com.png';
 import Explore from './sightbar_dropdown_components/Explore';
 import Plans from './sightbar_dropdown_components/Plans';
 import Setting from './sightbar_dropdown_components/Setting';
+import MyAccountDropdown from './sightbar_dropdown_components/MyAccountDropdown';
 import Info from './sightbar_dropdown_components/Info';
+import Auth from '../auth';
+import { FaCircleUser } from 'react-icons/fa6';
+import { BiSolidCrown } from "react-icons/bi";
+import { IoChevronForwardOutline } from 'react-icons/io5';
 const SightBarModal = ({ isOpen, onClose }) => {
+  const isAuthenticated = Auth.isAuthenticated();
+  const storedUserName = sessionStorage.getItem('userInfoN');
   useEffect(() => {
     const closeOnOutsideClick = (e) => {
       if (e.target.classList.contains('sight-bar-modal')) {
@@ -28,26 +36,61 @@ const SightBarModal = ({ isOpen, onClose }) => {
 
 
   return (
-    <div  className={`sight-bar-modal ${isOpen ? 'open' : ''}`}>
+    <div className={`sight-bar-modal ${isOpen ? 'open' : ''}`}>
       <button className="close-button" onClick={onClose}>
         <AiFillCloseCircle />
       </button>
       <div className="sight-bar-modal-content">
+        <div className="sight-auth-head">
+          <div className="sight-bar-header">
+            <button className="sight-close-btn" onClick={onClose}>
+              <AiFillCloseCircle />
+            </button>
+            <div className="logo">
+              <Link to="/" onClick={handleCloseModal}>
+                <img className="logo" src={logo} alt="Logo" />
+              </Link>
+            </div>
+          </div>
+          {isAuthenticated ? (
+            <div className="uma-sec">
+              <Link onClick={handleCloseModal} to="/account/profile" className="umc-head">
+                <div className="umc-pi-un">
+                  <FaCircleUser className="umc-icon" />
+                  <p className="umc-name">{storedUserName}</p>
+                </div>
+                <IoChevronForwardOutline className="umc-icon-ar" />
+              </Link>
+            </div>
+          ) : (
+            <div className="bp-dtn-3">
+              <button className='bp-btn'><BiSolidCrown className='dp-btn-icon' /> BUY PLAN</button>
+              <Link onClick={handleCloseModal} to="/login" className='lin-btn'>Login</Link>
+            </div>
 
-        <div style={{ marginTop: '10px', paddingLeft: '15px' }}><Link onClick={handleCloseModal} style={{ textDecoration: 'none', fontSize: '18px', color: 'white' }} to='/'>Home</Link></div>
-        <hr className='tab-item-hr' />
-        <Explore onClose={handleCloseModal}/>
-        <hr className='tab-item-hr' />
-        <Plans onClose={handleCloseModal}/>
-        <hr className='tab-item-hr' />
-        <Setting onClose={handleCloseModal}/>
-        <hr className='tab-item-hr' />
-        <div style={{ marginTop: '10px', paddingLeft: '15px' }}><Link onClick={handleCloseModal} style={{ textDecoration: 'none', fontSize: '16px', color: 'white' }} to='/Refer_Earn_Discount'>Refer and Earn and Discount</Link></div>
-        <hr className='tab-item-hr' />
-        <Info onClose={handleCloseModal}/>
-        <hr className='tab-item-hr' />
-        <div style={{ marginTop: '10px', textAlign: 'center' }}>Version 4.6.3</div>
-
+          )}
+        </div>
+        <div className="sight-scroll-pert">
+          <div style={{ marginTop: '10px', paddingLeft: '15px' }}><Link onClick={handleCloseModal} style={{ textDecoration: 'none', fontSize: '18px', color: 'white' }} to='/'>Home</Link></div>
+          <hr className='tab-item-hr' />
+          <Explore onClose={handleCloseModal} />
+          <hr className='tab-item-hr' />
+          <Plans onClose={handleCloseModal} />
+          <hr className='tab-item-hr' />
+          <Setting onClose={handleCloseModal} />
+          <hr className='tab-item-hr' />
+          {isAuthenticated && (
+            <div className="op-hi-drop">
+              <MyAccountDropdown onClose={handleCloseModal} />
+              <hr className="tab-item-hr" />
+            </div>
+          )}
+          <div style={{ marginTop: '10px', paddingLeft: '15px' }}><Link onClick={handleCloseModal} style={{ textDecoration: 'none', fontSize: '16px', color: 'white' }} to='/Refer_Earn_Discount'>Refer and Earn and Discount</Link></div>
+          <hr className='tab-item-hr' />
+          <Info onClose={handleCloseModal} />
+          <hr className='tab-item-hr' />
+          <div style={{ marginTop: '10px', textAlign: 'center' }}>Version 4.6.3</div>
+        </div>
       </div>
 
     </div>
---
+++ src/components/sightbar_dropdown_components/MyAccountDropdown.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { IoChevronDown, IoChevronUp } from 'react-icons/io5';
+import Auth from '../../auth';
+
+function MyAccountDropdown({onClose}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const isAuthenticated = Auth.isAuthenticated();
+  const navigate = useNavigate();
+
+  const toggleUserDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const userDropcontent = [
+    { id: 1, name: 'My Watchlist', path: '/account/watchlist' },
+    { id: 2, name: 'My Subscriptions', path: '/account/subscription' },
+    { id: 3, name: 'My Rentals', path: '/account/rentals' },
+    { id: 4, name: 'My Transactions', path: '/account/transaction' },
+    
+  ];
+
+  const handleOptionClick = (path) => {
+    // setIsOpen(false);
+    onClose();
+    navigate(path); 
+  };
+
+  const handleLogout = () => {
+    Auth.logout();
+    onClose();
+    navigate('/');
+  };
+
+  if (!isAuthenticated) {
+    return null; // If not authenticated, don't render the modal dropdown
+  }
+
+  return (
+    <div className={`sight-Userdropdown Userdropdown ${isOpen ? 'active' : ''}`}>
+      <div className="Userdropdown-toggle" onClick={toggleUserDropdown}>
+        My Account {isOpen ? <IoChevronUp /> : <IoChevronDown />}
+      </div>
+      {isOpen && (
+        <div className="Userdropdown-content">
+          {userDropcontent.map((option) => (
+            <div
+              key={option.id}
+              className="Userdropdown-link"
+              onClick={() => {handleOptionClick(option.path)
+              }}
+            >
+              {option.name}
+            </div>
+          ))}
+          {isAuthenticated && (
+            <div className="Userdropdown-link" onClick={handleLogout}>
+              Log out
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MyAccountDropdown
+
+
